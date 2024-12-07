ad2ce4c50dda1c12e968b2e889b4482fe708b638
Fontlos <fontlos@fontlos.com>
2024-12-08 00:22:53 +0800

    feat: parse BoyaStatistic

--- src/api/boya/query_statistic.rs
+++ src/api/boya/query_statistic.rs
@@ -1,14 +1,69 @@
+use serde::{Deserialize, Deserializer};
+
 use crate::{Session, SessionError};
+
+#[derive(Deserialize)]
+struct BoyaStatisticWrapper {
+    #[serde(deserialize_with = "deserialize_boya_statistics")]
+    data: BoyaStatistic,
+}
+
+fn deserialize_boya_statistics<'de, D>(deserializer: D) -> Result<BoyaStatistic, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    #[derive(Deserialize)]
+    struct Intermediate {
+        statistical: IntermediateBoyaStatisticData,
+    }
+
+    #[derive(Deserialize)]
+    struct IntermediateBoyaStatisticData {
+        #[serde(rename = "60|博雅课程")]
+        data: BoyaStatistic,
+    }
+
+    let intermediate = Intermediate::deserialize(deserializer)?;
+
+    Ok(intermediate.statistical.data)
+}
+
+#[derive(Debug, Deserialize)]
+pub struct BoyaStatistic {
+    #[serde(rename = "55|德育")]
+    pub deyu: BoyaAssessment,
+    #[serde(rename = "56|美育")]
+    pub meiyu: BoyaAssessment,
+    #[serde(rename = "57|劳动教育")]
+    pub laoyu: BoyaAssessment,
+    #[serde(rename = "58|安全健康")]
+    pub anquan: BoyaAssessment,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct BoyaAssessment {
+    #[serde(rename = "assessmentCount")]
+    pub require: u8,
+    #[serde(rename = "selectAssessmentCount")]
+    pub select: u8,
+    #[serde(rename = "completeAssessmentCount")]
+    pub complete: u8,
+    #[serde(rename = "failAssessmentCount")]
+    pub fail: u8,
+    #[serde(rename = "undoneAssessmentCount")]
+    pub undone: u8,
+}
 
 impl Session {
     /// # Query Statistic
     /// - Need: [`boya_login`](#method.boya_login)
     /// - Input: Token from [`boya_login`](#method.boya_login)
-    pub async fn boya_query_statistic(&self, token: &str) -> Result<String, SessionError> {
+    pub async fn boya_query_statistic(&self, token: &str) -> Result<BoyaStatistic, SessionError> {
         let query = "{}";
         let url = "https://bykc.buaa.edu.cn/sscv/queryStatisticByUserId";
         let res = self.boya_universal_request(query, url, token).await?;
-        Ok(res)
+        let res = serde_json::from_str::<BoyaStatisticWrapper>(&res)?;
+        Ok(res.data)
     }
 }
 
@@ -23,7 +78,7 @@ async fn test_boya_query_statistic() {
 
     let token = session.boya_login().await.unwrap();
     let res = session.boya_query_statistic(&token).await.unwrap();
-    println!("{}", res);
+    println!("{:?}", res);
 
     session.save();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ pub use api::{
     boya::{
         query_course::{BoyaCampus, BoyaCourse, BoyaKind, BoyaTime},
         query_selected::BoyaSelected,
+        query_statistic::{BoyaAssessment, BoyaStatistic},
     },
     class::{ClassCourse, ClassSchedule},
     spoc::{SpocSchedule, SpocTimeRange, SpocWeek},
