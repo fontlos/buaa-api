0447a5f2abae69170a9b7c91341d41517e9551ae
Fontlos <fontlos@fontlos.com>
2025-05-15 20:20:48 +0800

    feat: Add with_auth for Context

--- src/context.rs
+++ src/context.rs
@@ -52,6 +52,39 @@ impl Context {
         }
     }
 
+    /// Initialize with authentication data (credentials and cookies) from specified directory.
+    ///
+    /// This will attempt to load:
+    /// - Cookies from `./dir/cookies.json`
+    /// - Credentials from `./dir/cred.json`
+    ///
+    /// If either file doesn't exist or fails to load, default values will be used instead.
+    #[cfg(not(any(target_arch = "wasm32", target_arch = "wasm64")))]
+    pub fn with_auth<P: AsRef<Path>>(dir: P) -> Context<SSO> {
+        let cookies_path = dir.as_ref().join("cookies.json");
+        let cookies = Arc::new(AtomicCookieStore::new(
+            AtomicCookieStore::from_file(cookies_path),
+        ));
+        let cred_path = dir.as_ref().join("cred.json");
+        let cred = CredentialStore::from_file(cred_path);
+
+        let mut header = HeaderMap::new();
+        header.insert(HeaderName::from_bytes(b"User-Agent").unwrap(), HeaderValue::from_bytes(b"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0").unwrap());
+
+        let client = Client::builder()
+            .default_headers(header)
+            .cookie_provider(cookies.clone())
+            .build()
+            .unwrap();
+
+        Context {
+            client,
+            cookies,
+            cred: AtomicCell::new(cred),
+            _marker: PhantomData,
+        }
+    }
+
     pub fn set_account(&self, username: &str, password: &str) {
         self.cred.update(|c| {
             c.username = Some(username.to_string());
@@ -84,30 +117,54 @@ impl Context {
         self.cred.store(cred);
     }
 
-    /// Load cookies file to set Session cookies and set `cookie_path`, if the path is not exist, it will create a new file, but It won't be saved until you call `save` method
-    #[cfg(not(any(target_arch = "wasm32", target_arch = "wasm64")))]
-    pub fn with_cookies<P: AsRef<Path>>(&self, path: P) {
-        let cookie = AtomicCookieStore::from_file(path);
-        self.set_cookies(cookie);
+    pub fn get_cookies(&self) -> &AtomicCookieStore {
+        &self.cookies
     }
 
-    /// Load config from path, if the path is not exist or parse failed, it will return a default one
+    pub fn get_cred(&self) -> &CredentialStore {
+        self.cred.load()
+    }
+
+    /// Load authentication data (credentials and cookies) from specified directory.
+    ///
+    /// This will attempt to load:
+    /// - Cookies from `./dir/cookies.json`
+    /// - Credentials from `./dir/cred.json`
+    ///
+    /// If either file doesn't exist or fails to load, default values will be used instead.
+    /// For more precise control over loading behavior, you can manually construct and set the auth data:
+    /// ```
+    /// let context = Context::new();
+    /// let cookie = AtomicCookieStore::from_file(path);
+    /// context.set_cookies(cookie);
+    /// ```
     #[cfg(not(any(target_arch = "wasm32", target_arch = "wasm64")))]
-    pub fn with_cred<P: AsRef<Path>>(&self, path: P) {
-        let cred = CredentialStore::from_file(path);
+    pub fn load_auth<P: AsRef<Path>>(&self, dir: P) {
+        let cookies_path = dir.as_ref().join("cookies.json");
+        let cookies = AtomicCookieStore::from_file(cookies_path);
+        let cred_path = dir.as_ref().join("cred.json");
+        let cred = CredentialStore::from_file(cred_path);
+        self.set_cookies(cookies);
         self.set_cred(cred);
     }
 
-    /// save cookies manually
+    /// Save authentication data (credentials and cookies) to specified directory.
+    ///
+    /// This will attempt to save:
+    /// - Cookies to `./dir/cookies.json`
+    /// - Credentials to `./dir/cred.json`
+    ///
+    /// For more precise control over loading behavior, you can manually construct and set the auth data:
+    /// ```
+    /// let cookies = context.get_cookies();
+    /// cookies.to_file(path);
+    /// ```
     #[cfg(not(any(target_arch = "wasm32", target_arch = "wasm64")))]
-    pub fn save_cookie<P: AsRef<Path>>(&self, path: P) {
-        self.cookies.to_file(path);
-    }
-
-    /// save config manually
-    #[cfg(not(any(target_arch = "wasm32", target_arch = "wasm64")))]
-    pub fn save_cred<P: AsRef<Path>>(&self, path: P) {
-        self.cred.load().to_file(path);
+    pub fn save_auth<P: AsRef<Path>>(&self, dir: P) {
+        let cookies_path = dir.as_ref().join("cookies.json");
+        let cred_path = dir.as_ref().join("cred.json");
+        self.get_cookies().to_file(cookies_path);
+        self.get_cred().to_file(cred_path);
     }
 }
 
