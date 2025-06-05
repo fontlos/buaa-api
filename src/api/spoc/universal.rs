1760560f39e79639b7de93b7731f55c655513b65
Fontlos <fontlos@fontlos.com>
2025-06-05 23:33:44 +0800

    feat: Set Spoc API expiration time as 24 hours

--- src/api/spoc/universal.rs
+++ src/api/spoc/universal.rs
@@ -11,6 +11,10 @@ struct SpocState {
 
 impl super::SpocAPI {
     pub async fn universal_request(&self, query: &str, url: &str) -> crate::Result<String> {
+        // 因为我们可以知道 Token 是否过期, 我们这里只完成保守的刷新, 仅在 Token 超出我们预期时刷新 Token
+        if self.policy.load().is_auto() && self.cred.load().spoc_token.is_expired() {
+            self.login().await?;
+        }
         let cred = self.cred.load();
         let token = match cred.spoc_token.value() {
             Some(t) => t,
