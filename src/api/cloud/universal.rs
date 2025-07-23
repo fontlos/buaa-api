2a895a302c24718ebe7e17f0f59661d3b3eec1df
Fontlos <fontlos@fontlos.com>
2025-07-23 12:03:15 +0800

    refactor: Remove method arg in universal_request

--- src/api/cloud/universal.rs
+++ src/api/cloud/universal.rs
@@ -1,15 +1,9 @@
-use reqwest::Method;
 use serde_json::Value;
 
 use crate::Error;
 
 impl super::CloudAPI {
-    pub async fn universal_request(
-        &self,
-        url: &str,
-        data: &Value,
-        method: Method,
-    ) -> crate::Result<String> {
+    pub async fn universal_request(&self, url: &str, data: &Value) -> crate::Result<String> {
         // 首先尝试获取 token, 如果没有就可以直接返回了
         let cred = &self.cred.load().cloud_token;
         // 因为我们可以知道 Token 是否过期, 我们这里只完成保守的刷新, 仅在 Token 超出我们预期时刷新 Token
@@ -21,11 +15,7 @@ impl super::CloudAPI {
             None => return Err(Error::APIError("No Cloud Token".to_string())),
         };
 
-        let res = match method {
-            Method::GET => self.get(url).bearer_auth(token).send().await?,
-            Method::POST => self.post(url).bearer_auth(token).json(data).send().await?,
-            _ => return Err(Error::APIError("Unsupported Method".to_string())),
-        };
+        let res = self.post(url).bearer_auth(token).json(data).send().await?;
         let text = res.text().await?;
 
         Ok(text)
