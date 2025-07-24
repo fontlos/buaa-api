e20a664e514e0c644487d2980eb31f5813dbd53b
Fontlos <fontlos@fontlos.com>
2025-07-24 11:09:09 +0800

    feat: Add get_muti_download_url func to get a download URL of a zip package for multiple files

--- src/api/cloud/download.rs
+++ src/api/cloud/download.rs
@@ -18,6 +18,39 @@ impl super::CloudAPI {
 
         Ok(res.to_string())
     }
+
+    /// Get a download URL of a zip package for multiple files.
+    pub async fn get_muti_download_url(&self, items: &[CloudItem]) -> crate::Result<String> {
+        let url = "https://bhpan.buaa.edu.cn/api/open-doc/v1/file-download";
+        let ids: Vec<_> = items.iter().map(|item| {
+            // 从文件路径 id 反向找到文件 id, 找不到就用原 id, 这不会引起错误, 只会导致无法下载这个文件
+            // 为什么同样是下载, 单个文件就用完整 id, 多个文件就用文件 id, 那证明文件 id 就够用了, 为什么这样设计
+            let file_id = match item.id.rfind('/') {
+                Some(idx) => &item.id[idx + 1..],
+                None => &item.id,
+            };
+            serde_json::json!({ "id": file_id })
+        }).collect();
+        let data = serde_json::json!({
+            "name": "download.zip",
+            "doc": ids
+        });
+        let text = self.universal_request(&url, &data).await?;
+        let raw_url = match utils::get_value_by_lable(&text, "address\":\"", "\"") {
+            Some(url) => url,
+            None => return Err(crate::Error::APIError("Failed to get multi download url".to_string())),
+        };
+
+        // 在获取下载链接请求发出后获取 cred, 通过其自动刷新机制保证 token 正常情况是存在的
+        let cred = match self.cred.load().cloud_token.value() {
+            Some(token) => token,
+            None => return Err(crate::Error::APIError("No cloud token found".to_string())),
+        };
+
+        let url = format!("{}?token={}", raw_url, cred);
+
+        Ok(url)
+    }
 }
 
 #[cfg(test)]
@@ -33,7 +66,8 @@ mod tests {
 
         let user_dir = cloud.get_user_dir_id().await.unwrap();
         let list = cloud.list_dir(&user_dir).await.unwrap();
-        let download_url = cloud.get_download_url(&list.files[0]).await.unwrap();
+        // let download_url = cloud.get_download_url(&list.files[0]).await.unwrap();
+        let download_url = cloud.get_muti_download_url(&list.files).await.unwrap();
 
         println!("download_url: {download_url}");
 
