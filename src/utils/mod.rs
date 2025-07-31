d6fcd07226e0baa55a85556a7829ae5453d2f5a1
Fontlos <fontlos@fontlos.com>
2025-07-31 13:28:53 +0800

    refactor: Move utils/wifi.rs to api/wifi/utils.rs

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -5,8 +5,3 @@ pub(crate) use str::{get_value_by_lable, get_values_by_lable};
 
 mod time;
 pub use time::*;
-
-#[cfg(not(any(target_arch = "wasm32", target_arch = "wasm64")))]
-mod wifi;
-#[cfg(not(any(target_arch = "wasm32", target_arch = "wasm64")))]
-pub use wifi::*;
