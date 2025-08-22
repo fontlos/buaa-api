c10f3245fcd780f49dbdea529a091943d8dfb959
Fontlos <56759523+fontlos@users.noreply.github.com>
2025-08-22 19:53:30 +0800

    refactor: Use generics for `query` in `universal_request` in BoyaApi (#73)

--- src/api/boya/query.rs
+++ src/api/boya/query.rs
@@ -1,25 +1,30 @@
 use time::Date;
 
 use super::{
-    _BoyaCourses, _BoyaDetail, _BoyaSelecteds, _BoyaStatistics, BoyaSignRule, BoyaCourse,
-    BoyaSelected, BoyaStatistic,
+    _BoyaCourses, _BoyaDetail, _BoyaSelecteds, _BoyaStatistics, BoyaCourse, BoyaSelected,
+    BoyaSignRule, BoyaStatistic,
 };
 
 impl super::BoyaApi {
     /// # Query Course
     pub async fn query_course(&self) -> crate::Result<Vec<BoyaCourse>> {
-        let query = "{\"pageNumber\":1,\"pageSize\":10}";
+        let query = serde_json::json!({
+            "pageNumber": 1,
+            "pageSize": 10,
+        });
         // TODO: VPN 方法使用下面的 URL, 但我还没想好怎么分组
         // https://d.buaa.edu.cn/https/77726476706e69737468656265737421f2ee4a9f69327d517f468ca88d1b203b/sscv/queryStudentSemesterCourseByPage
         let url = "https://bykc.buaa.edu.cn/sscv/queryStudentSemesterCourseByPage";
-        let res = self.universal_request(url, query).await?;
+        let res = self.universal_request(url, &query).await?;
         let res = serde_json::from_str::<_BoyaCourses>(&res)?;
         Ok(res.data)
     }
 
     // 查询签到规则, 包括签到签退的时间地点
     pub async fn query_sign_rule(&self, id: u32) -> crate::Result<Option<BoyaSignRule>> {
-        let query = format!("{{\"id\":{id}}}");
+        let query = serde_json::json!({
+            "id": id,
+        });
         let url = "https://bykc.buaa.edu.cn/sscv/queryCourseById";
         let res = self.universal_request(url, &query).await?;
         let res = serde_json::from_str::<_BoyaDetail>(&res)?;
@@ -29,8 +34,11 @@ impl super::BoyaApi {
     /// # Query Selected Courses
     /// Date should like `year-month-day`
     pub async fn query_selected(&self, start: Date, end: Date) -> crate::Result<Vec<BoyaSelected>> {
-        let query =
-            format!("{{\"startDate\":\"{start} 00:00:00\",\"endDate\":\"{end} 00:00:00\"}}");
+        // TODO: 查询其他时间范围也没用, 找个接口查询本学期时间范围, 封装进去无需参数算了
+        let query = serde_json::json!({
+            "startDate": format!("{} 00:00:00", start),
+            "endDate": format!("{} 00:00:00", end),
+        });
         // TODO: VPN 方法使用下面的 URL, 但我还没想好怎么分组
         // https://d.buaa.edu.cn/https/77726476706e69737468656265737421f2ee4a9f69327d517f468ca88d1b203b/sscv/queryChosenCourse
         let url = "https://bykc.buaa.edu.cn/sscv/queryChosenCourse";
@@ -41,11 +49,11 @@ impl super::BoyaApi {
 
     /// # Query Statistic
     pub async fn query_statistic(&self) -> crate::Result<BoyaStatistic> {
-        let query = "{}";
+        let query = serde_json::json!({});
         // TODO: VPN 方法使用下面的 URL, 但我还没想好怎么分组
         // https://d.buaa.edu.cn/https/77726476706e69737468656265737421f2ee4a9f69327d517f468ca88d1b203b/sscv/queryStatisticByUserId
         let url = "https://bykc.buaa.edu.cn/sscv/queryStatisticByUserId";
-        let res = self.universal_request(url, query).await?;
+        let res = self.universal_request(url, &query).await?;
         let res = serde_json::from_str::<_BoyaStatistics>(&res)?;
         Ok(res.data)
     }
