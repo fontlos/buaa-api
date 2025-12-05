6b0927a0098581d65cc30a1dd62d2e49e7883957
Fontlos <fontlos@fontlos.com>
2025-12-05 12:44:09 +0800

    feat: Add `AppApi` Group

---
+++ src/api/app/mod.rs
@@ -0,0 +1,20 @@
+//! # BUAA App API
+
+/// # BUAA App API Group
+///
+/// Obtain a context view via [`Context.app()`],
+/// then call specific APIs through this grouping.
+///
+/// ## Examples
+///
+/// ```
+/// let ctx = Context::new();
+/// let app = ctx.app();
+/// app.login().await?;
+/// ```
+///
+/// ## Note
+///
+/// All API groups share the same underlying context -
+/// modifications will be instantly visible across all groups.
+pub type AppApi = crate::Context<super::App>;
