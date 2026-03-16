4a8c5ed8469be2a403745d59510a1b8e534a065f
Fontlos <fontlos@fontlos.com>
2026-03-16 19:17:55 +0800

    feat(live)!: Add `get_week_schedule`
    Remove `get_user_id`

--- src/api/live/data.rs
+++ src/api/live/data.rs
@@ -1,4 +1,4 @@
-use serde::Serialize;
+use serde::{Deserialize, Deserializer, Serialize};
 
 /// Request Body Payload
 pub enum Payload<'a, P: Serialize + ?Sized> {
@@ -8,4 +8,48 @@ pub enum Payload<'a, P: Serialize + ?Sized> {
     Json(&'a P),
     /// No data
     Empty,
+}
+
+// 经典司马接口每个响应体都不一样, 直接解析
+// {"code":0,"msg":"success","list":{OBJ,OBJ}}
+// {"code":10000,"message":"操作成功","data":{}}
+// {"success":true,"result":{"code":200,"msg":"", list:[]}}
+pub(super) struct Data<T>(pub T);
+
+/// Schedule info
+#[derive(Debug, Clone, Deserialize)]
+pub struct Schedule {
+    /// Course ID
+    pub course_id: String,
+    /// Live ID
+    #[serde(rename = "id")]
+    pub live_id: String,
+    /// Course name
+    #[serde(rename = "course_title")]
+    pub name: String,
+    /// Teacher name
+    #[serde(rename = "teacher_name")]
+    pub teacher: String,
+}
+
+impl<'de> Deserialize<'de> for Data<[Vec<Schedule>; 7]> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        #[derive(Deserialize)]
+        struct I {
+            result: J,
+        }
+        #[derive(Deserialize)]
+        struct J {
+            list: [K; 7],
+        }
+        #[derive(Deserialize)]
+        struct K {
+            course: Vec<Schedule>,
+        }
+        let i = I::deserialize(deserializer)?;
+        Ok(Data(i.result.list.map(|k| k.course)))
+    }
 }
