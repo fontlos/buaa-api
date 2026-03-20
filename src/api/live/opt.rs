c96c21cc710aae48f19b24942b3899b2a926d573
Fontlos <56759523+fontlos@users.noreply.github.com>
2026-03-20 10:38:18 +0800

    refactor(live): Use `Week` in `get_week_schedule` (#279)

--- src/api/live/opt.rs
+++ src/api/live/opt.rs
@@ -1,17 +1,14 @@
 use reqwest::Method;
 
 use crate::Error;
+use crate::utils::time::Week;
 
 use super::data::{Data, Payload, Schedule};
 
 // 什么纸张司马设计每个接口返回格式都不一样啊
 impl super::LiveApi {
     /// # Get Week Schedule
-    pub async fn get_week_schedule(
-        &self,
-        start: &str,
-        end: &str,
-    ) -> crate::Result<[Vec<Schedule>; 7]> {
+    pub async fn get_week_schedule(&self, week: &Week) -> crate::Result<[Vec<Schedule>; 7]> {
         // 日接口更难看, 上下午按对象存储
         // https://classroom.msa.buaa.edu.cn/courseapi/v2/course-live/get-my-course-day?day=<DATE>
         // {"code":0,"msg":"success","list":{OBJ,OBJ}}
@@ -19,7 +16,10 @@ impl super::LiveApi {
         // user_id 并不重要, 来自
         // https://classroom.msa.buaa.edu.cn/consoleapi/v2/user/group-user
         // {"code":10000,"message":"操作成功","data":{}}, id 字段
-        let query = [("start_at", start), ("end_at", end)];
+        let query = [
+            ("start_at", week.start.to_date2()),
+            ("end_at", week.end.to_date2()),
+        ];
         let payload = Payload::Query(&query);
         // {"success":true,"result":{"code":200,"msg":"", list:[]}}, 七个元素
         // success 似乎总是 true, 但 code 可能是 400
