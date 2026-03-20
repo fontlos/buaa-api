fccb0938b2a86933f5ab25b7a3e0e257029a1dc4
Fontlos <fontlos@fontlos.com>
2026-03-20 10:36:45 +0800

    refactor(live): Use `Week` in `get_week_schedule`

--- examples/live.rs
+++ examples/live.rs
@@ -5,14 +5,14 @@ mod tests {
     #[ignore]
     #[tokio::test]
     async fn test_live() {
+        use buaa_api::time::Week;
         let context = Context::with_auth("./data").unwrap();
 
         let live = context.live();
 
-        let schedule = live
-            .get_week_schedule("2026-03-09", "2026-03-15")
-            .await
-            .unwrap();
+        let week = Week::current();
+
+        let schedule = live.get_week_schedule(&week).await.unwrap();
         println!("Courses: {:?}", schedule);
 
         context.save_auth("./data").unwrap();
