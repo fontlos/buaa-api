88183943a0fbd6d1f1fa21888f680bf4ee2c9eac
Fontlos <56759523+fontlos@users.noreply.github.com>
2026-05-21 18:19:20 +0800

    feat(class): Auto switch to VPN (#308)

--- src/api/class/core.rs
+++ src/api/class/core.rs
@@ -1,7 +1,7 @@
 use bytes::Bytes;
 use serde::Serialize;
 
-use crate::api::{Class, Sso};
+use crate::api::{Class, Sso, Vpn};
 use crate::error::Error;
 use crate::{crypto, utils};
 
@@ -10,14 +10,35 @@ use crate::{crypto, utils};
 const CLASS_DES_KEY: &[u8] = b"Jyd#351*";
 
 impl super::ClassApi {
+    fn url(is_vpn: bool, port: u16, path: &str) -> String {
+        if is_vpn {
+            format!(
+                "https://d.buaa.edu.cn/https-8347/77726476706e69737468656265737421f9f44d9d342326526b0988e29d51367ba018/{path}"
+            )
+        } else {
+            format!("https://iclass.buaa.edu.cn:{port}/{path}")
+        }
+    }
+
     /// # Login to ClassApi
     pub async fn login(&self) -> crate::Result<()> {
+        // 注意, Class 登陆状态是可随意复写的, 调用一次复写一次
+        // 再加上 VPN 模式和普通模式的 Cred 和 Cookie 是可以互相用的
+        // 也就是说唯一需要仔细处理的地方就是 SsoAPI::login_vpn
+        // 确保在需要的时刻调用它以免 VPN 模式下 Class 登录流程失败
+
+        let is_vpn = !utils::net::is_on_campus_network();
+        // 只在需要时刷新 VPN 的 SSO
+        if is_vpn && self.cred.load().is_expired::<Vpn>() {
+            self.api::<Sso>().login_vpn().await?;
+        }
         if self.cred.load().is_expired::<Sso>() {
+            // 在任何情况下, 刷新 SSO 都是值得的
             self.api::<Sso>().login().await?;
         }
 
         // 获取 JSESSIONID
-        let url = "https://iclass.buaa.edu.cn:8346/";
+        let url = Self::url(is_vpn, 8346, "");
         let res = self.client.get(url).send().await?;
 
         // 整个这一次请求的意义存疑, 但也许是为了验证 loginName 是否有效
@@ -30,7 +51,7 @@ impl super::ClassApi {
         let url = crypto::bytes2hex(&url);
         let query = [("method", "html5GetPrivateUserInfo"), ("url", &url)];
         self.client
-            .get("https://iclass.buaa.edu.cn:8346/wc/auth/html5GetPrivateUserInfo")
+            .get(Self::url(is_vpn, 8346, "wc/auth/html5GetPrivateUserInfo"))
             .query(&query)
             .send()
             .await?;
@@ -50,7 +71,7 @@ impl super::ClassApi {
         // 很难想象能有这种错误发生
         let res = self
             .client
-            .get("https://iclass.buaa.edu.cn:8347/app/user/login.action")
+            .get(Self::url(is_vpn, 8346, "app/user/login.action"))
             .query(&query)
             .send()
             .await?
@@ -75,11 +96,18 @@ impl super::ClassApi {
     /// Universal Request for ClassApi (Internal)
     ///
     /// **Note**: `token` parameter is already included
-    pub(crate) async fn universal_request<P>(&self, url: &str, payload: &P) -> crate::Result<Bytes>
+    pub(crate) async fn universal_request<P>(
+        &self,
+        port: u16,
+        path: &str,
+        payload: &P,
+    ) -> crate::Result<Bytes>
     where
         P: Serialize + ?Sized,
     {
         let cred = self.cred.load();
+        // 注意, 这里无需特殊处理, VPN 模式和正常模式的 Cred 和 Cookie 是可以互相用的
+        // 也就是说唯一需要仔细处理的地方就是 ClassAPI::login 函数自己
         if cred.is_expired::<Class>() {
             self.login().await?;
         }
@@ -89,6 +117,9 @@ impl super::ClassApi {
         let (session, id) = token
             .split_once('@')
             .ok_or(Error::auth("Cannot split 'session' and 'id' token").with_label("Class"))?;
+
+        let is_vpn = !utils::net::is_on_campus_network();
+        let url = Self::url(is_vpn, port, path);
 
         // 在 URL 中硬编码 id
         let bytes = self
